91a02400fad4924f6f1782d8beb8f2baeabe16cd
Matthew Mazzola <mattm@microsoft.com>
2024-01-03 14:27:54 -0800

    Update install

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -26,19 +26,29 @@ conda create -n grit python=3.8 -y
 conda activate grit
 
 echo "Installing CUDA..."
-conda install -y -c nvidia cuda-nvcc
+conda install -y -c nvidia cuda-nvcc=11.3
 
 echo "Installing detectron2..."
-git clone https://github.com/facebookresearch/detectron2.git
-cd detectron2
-git checkout cc87e7ec
-pip install -e .
-cd ..
+# git clone https://github.com/facebookresearch/detectron2.git
+# cd detectron2
+# git checkout cc87e7ec
+# pip install -e .
+# cd ..
 
-echo "Downloading pretrained model..."
-mkdir models
+# https://github.com/facebookresearch/detectron2/releases
+python -m pip install detectron2==0.6 -f \
+  https://dl.fbaipublicfiles.com/detectron2/wheels/cu113/torch1.10/index.html
+
+mkdir -p models
 cd models
-wget https://datarelease.blob.core.windows.net/grit/models/grit_b_densecap_objectdet.pth
+
+if [ ! -f grit_b_densecap_objectdet.pth ]; then
+  echo "Downloading pretrained model..."
+  wget https://datarelease.blob.core.windows.net/grit/models/grit_b_densecap_objectdet.pth
+else
+  echo "Skipping download, pretrained model already exists."
+fi
+
 cd ..
 
 echo "Installing requirements..."
