06173d7e208a3f2f3d4176fb854de12a0154c3f6
Matthew Mazzola <mattm@microsoft.com>
2024-01-03 19:39:40 +0000

    Add .devcontainer and .blobfuse

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,42 @@
+git config --global safe.directory '*'
+git config --global core.editor "code --wait"
+git config --global pager.branch false
+
+# Set AZCOPY concurrency to auto
+echo "export AZCOPY_CONCURRENCY_VALUE=AUTO" >> ~/.zshrc
+echo "export AZCOPY_CONCURRENCY_VALUE=AUTO" >> ~/.bashrc
+
+# Add dotnet to PATH
+echo 'export PATH="$PATH:$HOME/.dotnet"' >> ~/.zshrc
+echo 'export PATH="$PATH:$HOME/.dotnet"' >> ~/.bashrc
+
+# Activate conda by default
+echo "source /home/vscode/miniconda3/bin/activate" >> ~/.zshrc
+echo "source /home/vscode/miniconda3/bin/activate" >> ~/.bashrc
+
+# Use grit environment by default
+echo "conda activate grit" >> ~/.zshrc
+echo "conda activate grit" >> ~/.bashrc
+
+# Activate conda on current shell
+source /home/vscode/miniconda3/bin/activate
+
+# Create and activate grit environment
+conda create -n grit python=3.8 -y
+conda activate grit
+
+# Install Nvidia Cuda Compiler
+conda install -y -c nvidia cuda-compiler
+
+pip install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+
+# Install detectron2
+git clone https://github.com/facebookresearch/detectron2.git
+cd detectron2
+git checkout cc87e7ec
+pip install -e .
+cd ..
+
+pip install -r requirements.txt
+
+echo "postCreateCommand.sh completed!"
